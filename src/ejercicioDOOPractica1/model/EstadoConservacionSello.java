df4164ebf0206c5943c3a1ba48b5b5467d971c29
Your Name <acolllen@myuax.com>
2025-11-03 10:22:38 +0100

    Aquí guardamos 3 cosas: - El extends a clase de Sello - La enumeración de Sello en clase separada - La clase java con lo requerido por el enunciado.

---
+++ src/ejercicioDOOPractica1/model/EstadoConservacionSello.java
@@ -0,0 +1,8 @@
+package ejercicioDOOPractica1.model;
+
+public enum EstadoConservacionSello {
+    U,
+    NSG,
+    NF,
+    N,
+}
---
+++ src/ejercicioDOOPractica1/model/Sello.java
@@ -0,0 +1,31 @@
+package ejercicioDOOPractica1.model;
+
+public class Sello extends ElementoColeccionable{
+    protected double altura;
+    protected double anchura;
+    protected String imagen;
+    protected EstadoConservacionSello estadoConservacionSello;
+
+
+    public Sello(String pais, String autoridadGobernante, int annus, String unidadMonetaria, double valor, int rareza, double precio) {
+        super (pais, autoridadGobernante, annus, unidadMonetaria, valor, rareza, precio);
+
+        this.altura = altura;
+        this.anchura = anchura;
+        this.imagen = imagen;
+        this.estadoConservacionSello = estadoConservacionSello;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public double getAnchura() {
+        return anchura;
+    }
+    public String getImagen() {
+        return imagen;
+    }
+    public EstadoConservacionSello getEstadoConservacionSello() {
+        return estadoConservacionSello;
+    }
+}
