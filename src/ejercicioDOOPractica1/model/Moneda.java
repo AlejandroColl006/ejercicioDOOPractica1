0b0a0cc0a612df5cd7454229ed9857de5bb9dd3b
Your Name <acolllen@myuax.com>
2025-11-03 09:26:50 +0100

    En la clase Moneda, hemos hecho todas las declaraciones de variables, hacemos el constructor, y para incluir todas las variables declaradas en la clase ElementoColeccionable introducimos un super, que es la clase padre. luego inciamos los atributos y más tarde con los getters y return con el valor. Algunas variables como grosor, diametro y peso estan en double porque los pesos no siempre son redondos (enteros) e igual con las distancias.

--- src/ejercicioDOOPractica1/model/Moneda.java
+++ src/ejercicioDOOPractica1/model/Moneda.java
@@ -3,5 +3,38 @@ package ejercicioDOOPractica1.model;
 public class Moneda extends ElementoColeccionable {
     protected String composicion;
     protected double peso;
+    protected double diametro;
+    protected double grosor;
+    protected EstadoConservacionMoneda estadoConservacionMoneda;
 
+    public Moneda (String pais, String autoridadGobernante, int annus, String unidadMonetaria,
+                   double valor, int rareza, double precio, String Composicion, double peso,
+                   double diametro, double grosor, EstadoConservacionMoneda estadoConservacionMoneda ) {
+
+        super(pais, autoridadGobernante, annus, unidadMonetaria, valor, rareza, precio);
+
+        this.composicion = Composicion;
+        this.peso = peso;
+        this.diametro = diametro;
+        this.grosor = grosor;
+        this.estadoConservacionMoneda = estadoConservacionMoneda;
+    }
+    public String getComposicion() {
+        return composicion;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public double getDiametro() {
+        return diametro;
+    }
+    public double getGrosor() {
+        return grosor;
+    }
+    public EstadoConservacionMoneda getEstadoConservacionMoneda() {
+        return estadoConservacionMoneda;
+    }
 }
+
+
+
