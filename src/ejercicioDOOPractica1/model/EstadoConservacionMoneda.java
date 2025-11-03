30eb2c3b7ee5e1bbcd3303a1625539f7cdf436a0
Your Name <acolllen@myuax.com>
2025-11-03 08:51:43 +0100

    En la clase Moneda, hemos hecho un par de declaraciones de variable como pide el enunciado, y hemos creado la clase aparte con la enumeración pedida.

---
+++ src/ejercicioDOOPractica1/model/EstadoConservacionMoneda.java
@@ -0,0 +1,11 @@
+package ejercicioDOOPractica1.model;
+
+public enum EstadoConservacionMoneda {
+    G,
+    VG,
+    F,
+    VF,
+    XF,
+    AU,
+    UNC
+}
---
+++ src/ejercicioDOOPractica1/model/Moneda.java
@@ -0,0 +1,7 @@
+package ejercicioDOOPractica1.model;
+
+public class Moneda extends ElementoColeccionable {
+    protected String composicion;
+    protected double peso;
+
+}
